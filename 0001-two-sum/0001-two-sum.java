8c4281308c24e43445fa85d32a05b88415d1bffc
Shiv_Core <shivashankrmali7@gmail.com>
2026-06-20 18:50:22 +0530

    Time: 2 ms (99.39%), Space: 47.5 MB (10.95%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,19 +1,17 @@
-import java.util.HashMap;
-class Solution 
-{
+class Solution {
     public int[] twoSum(int[] nums, int target) {
         HashMap<Integer, Integer> map = new HashMap<>();
-        
-        for (int i = 0; i < nums.length; i++) 
-        {
+
+        for (int i = 0; i < nums.length; i++) {
             int complement = target - nums[i];
-            
-            if (map.containsKey(complement))
-            {
-                return new int[] { map.get(complement), i };
+
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
             }
+
             map.put(nums[i], i);
         }
-        throw new IllegalArgumentException("No two sum solution");
+
+        return new int[]{};
     }
 }
